f30216889885cc275e0ad2381b8d77ec6540ff57
YgoFT <yagogabrielftonarque@gmail.com>
2025-04-16 10:23:51 -0300

    listarepetição2_5.c

--- listarepetição2_5.c
+++ listarepetição2_5.c
@@ -4,10 +4,12 @@
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
- //   int g, c=111;
     int g, c = rand() % 501;
+    printf("Adivinhe o número de 0 a 500 gerado aleatóriamente pelo código: ");
+    scanf("%d", &g);
     while(g != c)
     {
+        c = rand() % 501;
         printf("Adivinhe o número de 0 a 500 gerado aleatóriamente pelo código: ");
         scanf("%d", &g);
     }
