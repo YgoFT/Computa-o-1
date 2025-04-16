73fc390e32b1c489a21eb9eb395fe8d3838d6b23
YgoFT <yagogabrielftonarque@gmail.com>
2025-04-16 08:06:45 -0300

    exercício3.c

--- exercício3.c
+++ exercício3.c
@@ -5,12 +5,14 @@ int main()
     setlocale(LC_ALL, "Portuguese");
     int n=0;
     float media, a, b, c;
-    while (n<3)
+    printf("Defina uma quantia de repetições: ");
+    scanf("%d", &n);
+    while (n>0)
     {
         printf("escolha tres valores reais: ");
         scanf("%f %f %f", &a, &b, &c);
         media = (a*2+b*3+c*5)/10;
         printf("média = %.2f\n", media);
-        n++;
+        n--;
     }
 }
