44ec673fab0f5d8189e8ed8fd90fdda7ccbe3368
YgoFT <yagogabrielftonarque@gmail.com>
2025-04-23 09:07:53 -0300

    Update prova1_2.c

--- prova1_2.c
+++ prova1_2.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    float media_min, km, min, seg;
+    float media_min, media_seg, result, dif;
+    int min, km, seg;
     printf("Quantos km você percorreu?\nResposta: ");
-    scanf("%f", &km);
+    scanf("%d", &km);
     printf("Em quantos minutos você percorreu?\nResposta: ");
-    scanf("%f", &min);
+    scanf("%d", &min);
     printf("Em quantos segundos você percorreu?\nResposta: ");
-    scanf("%f", &seg);
-    media_min = (min+(seg/60))/km;
-    printf("Sua média em km/minuto é de %.2f\n", media_min);
+    scanf("%d", &seg);
+    media_min = min/km;
+    dif = min%km;
+    media_seg = ((dif*60)+seg)/(km*100);
+    result = media_min+media_seg;
+    printf("Sua média em km/s é de %.2f\n", result);
 }
